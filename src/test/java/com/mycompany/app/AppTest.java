2eb0eb6e5327386f3dcf355c62a54059ef3000e6
shodhan-simpleenergy <shodhan@simpleenergy.in>
2025-09-29 19:08:09 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,20 +1,42 @@
+// package com.mycompany.app;
+
+// import static org.junit.Assert.assertTrue;
+
+// import org.junit.Test;
+
+// /**
+//  * Unit test for simple App.
+//  */
+//  @Test
+// public void whenPalindrom_thenAccept() {
+//     Palindrome palindromeTester = new Palindrome();
+//     assertTrue(palindromeTester.isPalindrome("noon"));
+// }
+    
+// @Test
+// public void whenNearPalindrom_thenReject(){
+//     Palindrome palindromeTester = new Palindrome();
+//     assertFalse(palindromeTester.isPalindrome("neon"));
+// }
+
 package com.mycompany.app;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
- @Test
-public void whenPalindrom_thenAccept() {
-    Palindrome palindromeTester = new Palindrome();
-    assertTrue(palindromeTester.isPalindrome("noon"));
-}
+public class AppTest {
+
+    @Test
+    public void whenPalindrome_thenAccept() {
+        App palindromeTester = new App();
+        assertTrue(palindromeTester.isPalindrome("noon"));
+    }
     
-@Test
-public void whenNearPalindrom_thenReject(){
-    Palindrome palindromeTester = new Palindrome();
-    assertFalse(palindromeTester.isPalindrome("neon"));
+    @Test
+    public void whenNearPalindrome_thenReject() {
+        App palindromeTester = new App();
+        assertFalse(palindromeTester.isPalindrome("neon"));
+    }
 }
