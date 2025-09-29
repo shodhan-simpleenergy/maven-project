75cb6471d160ed6445142601946260854ab86898
shodhan-simpleenergy <shodhan@simpleenergy.in>
2025-09-29 19:03:17 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,16 +1,39 @@
+// package com.mycompany.app;
+
+// /**
+//  * Hello world!
+//  *
+//  */
+// public boolean isPalindrome(String inputString) {
+//     if (inputString.length() == 0) {
+//         return true;
+//     } else {
+//         char firstChar = inputString.charAt(0);
+//         char lastChar = inputString.charAt(inputString.length() - 1);
+//         String mid = inputString.substring(1, inputString.length() - 1);
+//         return (firstChar == lastChar) && isPalindrome(mid);
+//     }
+// }
+
 package com.mycompany.app;
 
-/**
- * Hello world!
- *
- */
-public boolean isPalindrome(String inputString) {
-    if (inputString.length() == 0) {
-        return true;
-    } else {
-        char firstChar = inputString.charAt(0);
-        char lastChar = inputString.charAt(inputString.length() - 1);
-        String mid = inputString.substring(1, inputString.length() - 1);
-        return (firstChar == lastChar) && isPalindrome(mid);
+public class App {
+
+    public boolean isPalindrome(String inputString) {
+        if (inputString.length() == 0) {
+            return true;
+        } else {
+            char firstChar = inputString.charAt(0);
+            char lastChar = inputString.charAt(inputString.length() - 1);
+            String mid = inputString.substring(1, inputString.length() - 1);
+            return (firstChar == lastChar) && isPalindrome(mid);
+        }
+    }
+
+    // Optional: main method to test
+    public static void main(String[] args) {
+        App app = new App();
+        String test = "racecar";
+        System.out.println(test + " is palindrome? " + app.isPalindrome(test));
     }
 }
